458626aa122c02aa69882acd27fb6e670094e98b
Allen Ben Philipose <allenalvin333@gmail.com>
2022-07-29 07:26:53 +0530

    programs

---
+++ 27.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    q_scores = student_marks[input()]
+    print("{0:.2f}".format(sum(q_scores)/(len(q_scores))))
