ab4307fcd2d1df803e38de5c71c73430a2551ce3
Allen Ben Philipose <allenalvin333@gmail.com>
2022-08-16 11:14:49 +0530

    programs

--- 43.py
+++ 43.py
@@ -1,1 +1,6 @@
 # https://www.hackerrank.com/challenges/itertools-product/problem
+
+from itertools import product
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+print(*product(x, y))
