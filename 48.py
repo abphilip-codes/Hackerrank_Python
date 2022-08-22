8f7346fcd05a2ad684a15eac6bd85093d5f2aeec
Allen Ben Philipose <allenalvin333@gmail.com>
2022-08-22 07:12:08 +0530

    programs

--- 48.py
+++ 48.py
@@ -1,2 +1,12 @@
 # https://www.hackerrank.com/challenges/defaultdict-tutorial/problem
 
+n, m = map(int, input().split())
+d = {}
+for z in range(n):
+    x = input()
+    d.setdefault(x, [])
+    d[x].append(z+1)
+
+for z in range(m):
+    x = input()
+    print(*d[x]) if(x in d) else print(-1)
