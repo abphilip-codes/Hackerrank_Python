f9d1d753cf76432e4108778cf3f1df4d4a30e9ce
Allen Ben Philipose <allenalvin333@gmail.com>
2022-08-28 15:48:28 +0530

    programs

---
+++ 54.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/no-idea/problem
+
+from collections import Counter
+
+n, m = map(int, input().split())
+
+c = Counter(list(map(int, input().split())))
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+k = 0
+
+for z, f in c.items():
+    if (z in a): k+=f
+    if (z in b): k-=f
+print(k)
