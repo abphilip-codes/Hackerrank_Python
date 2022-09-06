b799ba7f40082440a8b387b8620925b596cba1ad
Allen Ben Philipose <allenalvin333@gmail.com>
2022-09-06 09:56:57 +0530

    programs

--- 63.py
+++ 63.py
@@ -1,2 +1,5 @@
-# https://www.hackerrank.com/challenges/py-collections-deque/problem
+# https://www.hackerrank.com/challenges/compress-the-string/problem
 
+from itertools import groupby
+for z in [list(g) for _, g in groupby(input())]:
+    print("({}, {})".format(len(z), z[0]), end = ' ')
