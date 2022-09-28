c1fa1b5236a9bf450a59d0c43f6b92b8a611694d
Allen Ben Philipose <allenalvin333@gmail.com>
2022-09-28 07:39:02 +0530

    programs

--- 84.py
+++ 84.py
@@ -1,6 +1,15 @@
-# https://www.hackerrank.com/challenges/introduction-to-regex/problem
+# https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
 
-import re
+cube = lambda x: x**3
 
-n, r = int(input()), r"^[-+]?\d*[.]\d*$"
-[print(bool(re.match(r, input()))) for _ in range(n)]
+def fibonacci(n):
+    f0, f1 = 0, 1
+    fib = []
+    for _ in range(n):
+        fib.append(f0)
+        f0, f1 = f1, f0+f1
+    return fib
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
