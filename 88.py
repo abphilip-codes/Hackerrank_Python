5afda7bcef2881fc92b73af1c6deb0f8536fc951
Allen Ben Philipose <allenalvin333@gmail.com>
2022-10-02 16:52:29 +0530

    programs

--- 88.py
+++ 88.py
@@ -1,10 +1,9 @@
 # https://www.hackerrank.com/challenges/re-start-re-end/problem
 
 import re
-
 s, k = input(), input()
-indexes = [(m.start(), m.end() + (len(k) -1)) for m in re.finditer(rf"(?={k})", s)]
+x = [(m.start(), m.end() + (len(k) -1)) for m in re.finditer(rf"(?={k})", s)]
 
-if len(indexes) > 0:
-    for z in indexes: print(z)
-else: print((-1, -1))
+if(not x): print((-1, -1))
+else: 
+    for z in x: print(z)
