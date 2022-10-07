f6d3ee6cc345ff09e2d53d98bf0d413bb9629b08
Allen Ben Philipose <allenalvin333@gmail.com>
2022-10-07 07:25:07 +0530

    programs

--- 92.py
+++ 92.py
@@ -1,7 +1,19 @@
-# https://www.hackerrank.com/challenges/hex-color-code/problem
+# https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
 
 import re
 
-exp, k = r"[\s:](#[0-9A-Fa-f]{3,6})", []
-for _ in range(int(input())): k.extend(re.findall(exp, input()))
-print(*k, sep='\n')
+def fun(s):
+    return True if re.search("^[\w-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$", s) else False
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
