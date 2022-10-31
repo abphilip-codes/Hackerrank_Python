3d63e9989c6fb342ed896500b74fe863f0d9edf0
Allen Ben Philipose <allenalvin333@gmail.com>
2022-10-31 19:19:57 +0530

    sWAP cASE

--- 29.py
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/python-tuples/problem
-
-if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split())
-    print hash(tuple(integer_list))
