3da3641756d80b3fd187054085892621f5324715
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-15 09:51:02 +0530

    Regex Substitution

--- Regex_And_Parsing_06.py
+++ Regex_And_Parsing_06.py
@@ -2,9 +2,6 @@
 
 for i in range(int(input())):
     line = input()
-    
     while ' && ' in line or ' || ' in line:
         line = line.replace(" && ", " and ").replace(" || ", " or ")
-    
     print(line)
-
