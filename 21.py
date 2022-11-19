ce6b291ea094ebc5917f74212cb88de60f1afd71
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-19 11:28:09 +0530

    Min and Max

--- 21.py
+++
@@ -1,5 +0,0 @@
-# https://www.hackerrank.com/challenges/np-min-and-max/problem
-
-import numpy as np
-print(np.max(np.min(np.array([input().split() for _ in range(int(input().split()[0]))],int),axis=1)))
-
