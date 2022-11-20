c58102a2680e556e476156323c38cb3bd2ad2c4c
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-20 07:37:24 +0530

    Dot and Cross

--- 24.py
+++ 24.py
@@ -4,4 +4,3 @@ import numpy as np
 a = np.array(input().split(), int)
 b = np.array(input().split(), int)
 print(np.inner(a,b), np.outer(a,b), sep='\n')
-
--- Numpy_12.py
+++ Numpy_12.py
@@ -1,7 +1,7 @@
 # https://www.hackerrank.com/challenges/np-dot-and-cross/problem
 
 import numpy
-a=int(input())
-a1=numpy.array([list(map(int,input().split())) for _ in range(a)])
-a2=numpy.array([list(map(int,input().split())) for _ in range(a)])
+a = int(input())
+a1 = numpy.array([list(map(int,input().split())) for _ in range(a)])
+a2 = numpy.array([list(map(int,input().split())) for _ in range(a)])
 print(numpy.dot(a1,a2))
